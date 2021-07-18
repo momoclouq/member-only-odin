9cdc39b3de9d913b4637806a43fa348b85441541
momoclouq <phamhoangminh050899@gmail.com>
2021-07-18 20:36:21 +0700

    production version

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const compression = require("compression");
+var helmet = require('helmet');
 
 //database connection
 const mongoose = require("mongoose");
@@ -29,6 +31,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
